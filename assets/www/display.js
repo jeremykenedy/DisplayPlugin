d29af255dd7607502ea3b05dcce1e44a81662fc6
fesou <fesou@fesou>
2013-12-11 02:26:47 +0100

    fix minor problems

--- assets/www/display.js
+++ assets/www/display.js
@@ -3,7 +3,7 @@ var display = {
         cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
-            'DisplayPlugin', // mapped to our native Java class called "CalendarPlugin"
+            'Display', // mapped to our native Java class called "DisplayPlugin"
             'setBrightness', // with this action name
             [{                  // and this array of custom arguments to create our entry
                 "brightness": brightness
