2a9784b45d6c694ab38f72ae4814f0d961a615eb
fesou <fesou@fesou>
2014-01-03 12:15:47 +0100

    Let's get back to the action.

--- src/pl/edu/uj/tcs/student/tomaszwesolowski/Display.java
+++ src/pl/edu/uj/tcs/student/tomaszwesolowski/Display.java
@@ -12,6 +12,7 @@ import android.view.WindowManager;
 public class Display extends CordovaPlugin {
 	public static final String SET_BRIGHTNESS = "setBrightness";
 	public static final String SET_ORIENTATION = "setOrientation";
+	//public static final String SET_ORIENTATION = "setOrientation";
 
 	@Override
 	public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
@@ -48,6 +49,26 @@ public class Display extends CordovaPlugin {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
+					}
+				});
+
+				callbackContext.success();
+				return true;
+			}
+			else if (SET_BRIGHTNESS.equals(action)) {
+				final JSONObject arg_object = args.getJSONObject(0);
+
+				cordova.getActivity().runOnUiThread(new Runnable() {
+					public void run() {
+						// Main Code goes here
+						WindowManager.LayoutParams layout = cordova.getActivity().getWindow().getAttributes();
+						try {
+							layout.screenBrightness = (float) arg_object.getDouble("brightness");
+						} catch (JSONException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						cordova.getActivity().getWindow().setAttributes(layout);
 					}
 				});
 
