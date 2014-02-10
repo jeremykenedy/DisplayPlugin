f2ece4ca22877df4926b99a3215a1ab8174730f3
mrfesol <fesou@fesou>
2014-02-10 12:20:50 +0100

    TYPE_PRIORITY_PHONE

--- src/pl/edu/uj/tcs/student/tomaszwesolowski/Display.java
+++ src/pl/edu/uj/tcs/student/tomaszwesolowski/Display.java
@@ -182,7 +182,7 @@ public class Display extends CordovaPlugin {
 	                .getAttributes();
 	        layoutParams.format = PixelFormat.TRANSLUCENT;
 	        
-	        layoutParams.type=LayoutParams.TYPE_PHONE;
+	        layoutParams.type=LayoutParams.TYPE_PRIORITY_PHONE;
 	        layoutParams.flags=LayoutParams.FLAG_NOT_TOUCH_MODAL | LayoutParams.FLAG_NOT_FOCUSABLE;
 	        layoutParams.gravity=Gravity.LEFT|Gravity.TOP; 
 
